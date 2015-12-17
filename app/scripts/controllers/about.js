fe39a49ae8701e083ad8344b025ec6515380f5b8
Sebastien Gregoire <s.gregoire@outlook.com>
2015-12-17 10:27:14 +0100

    Ajout d'AngularJS

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webClientSideApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webClientSideApp
+ */
+angular.module('webClientSideApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webClientSideApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webClientSideApp
+ */
+angular.module('webClientSideApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
