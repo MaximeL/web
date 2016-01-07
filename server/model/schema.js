16adc8ff56af3dcabe11fd51bc9fff0b5b2b58c5
Romain Palmero <romainpalmero.dev@gmail.com>
2016-01-07 21:40:29 +0100

    Ajout du backend à branche dev

---
+++ server/model/schema.js
@@ -0,0 +1,13 @@
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+
+// var dbName = 'dbsound';
+// var port = '27017';
+// mongoose.connect('mongodb://localhost:' + port + '/' + dbName);
+
+// var Connection = new Schema({
+//     name: String,
+//     extension: String
+// }, {collection: 'sound', versionKey: false} );
+
+// module.exports = mongoose.model('Conn', Connection);
