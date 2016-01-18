3e7717c3d84000f78f3eb466244838957fac954c
Sebastien GREGOIRE <Sebastien GREGOIRE>
2016-01-18 16:34:12 +0100

    Intégration des test de routing user et pedal

--- server/test/routingTest.js
+++ server/test/routingTest.js
@@ -15,7 +15,9 @@ describe('Routing test', function () {
   var URL = 'http://localhost:3000';
   var TEST_DB = 'dbsound_test';
   var URL_NOTE = '/api/note/';
-  //var URL_COMMENT = '/api/comment/';
+  var URL_COMMENT = '/api/comment/';
+  var URL_USER = '/api/user/';
+  var URL_PEDAL = '/api/pedal/';
 
   // connection with the database
   before(function (done) {
@@ -100,7 +102,6 @@ describe('Routing test', function () {
     it('should correctly delete a note', function (done) {
       request(URL)
         .delete(URL_NOTE + id_created)
-        .send(noteBody)
         .expect('Content-type', 'application/json; charset=utf-8')
         .expect(200) //Status code success
         .end(function (err, res) {
@@ -191,7 +192,6 @@ describe('Routing test', function () {
     it('should correctly delete a comment', function (done) {
       request(URL)
         .delete(URL_COMMENT + id_created)
-        .send(commentBody)
         .expect('Content-type', 'application/json; charset=utf-8')
         .expect(200) //Status code success
         .end(function (err, res) {
@@ -212,9 +212,176 @@ describe('Routing test', function () {
    *  Test pour les Users
    *  --------------------------------------------------------------------------------------- */
 
+  describe('User API testing', function () {
+
+    var id_created = null;
+    var userBody = {
+      username: "test",
+      password: "alligator3"
+    };
+
+    // TEST POST
+    it('should correctly post a new user', function (done) {
+      request(URL)
+        .post(URL_USER)
+        .send(userBody)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(201) //Status code created
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          // Should.js fluent syntax applied
+          res.body.should.have.property('_id');
+          // recupere l'id du post pour tester le get par id
+          id_created = res.body._id;
+          res.body.username.should.equal("test");
+          res.body.password.should.not.have.property("password");
+          done();
+        });
+    });
+
+    // TEST PUT
+    it('should correctly update a user', function (done) {
+      userBody.username = 'update-test';
+      // TODO : Tester les pédales et les partages
+      request(URL)
+        .put(URL_USER + id_created)
+        .send(noteBody)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200) //Status code success
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          // Should.js fluent syntax applied
+          res.body._id.should.equal(id_created);
+          res.body.username.should.equal("update-test");
+          res.body.should.have.property("pedals");
+          res.body.should.have.property("shared");
+          res.body.should.not.have.property("password");
+          done();
+        });
+    });
+
+    // TEST GET PAR ID
+    it('should correctly get a user', function (done) {
+      request(URL)
+        .get(URL_USER + id_created)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200) //Status code success
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          res.body._id.should.equal(id_created);
+          res.body.username.should.equal("test");
+          // TODO : Check values
+          res.body.should.have.property("pedals");
+          res.body.should.have.property("shared");
+          res.body.should.not.have.property("password");
+          done();
+        });
+    });
+  });
+
   /** ---------------------------------------------------------------------------------------
    *  Test pour les Pedales
    *  --------------------------------------------------------------------------------------- */
 
+  describe('Pedal API testing', function () {
+    var id_owner = "aze";
+    var id_created = null;
+    var pedalBody = {
+      nom: "Ma pédale",
+      description: "Celle de Tonton",
+
+      owner: id_owner
+    };
+
+    // TEST POST
+    it('should correctly post a new note', function (done) {
+      request(URL)
+        .post(URL_PEDAL)
+        .send(pedalBody)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(201) //Status code created
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          // Should.js fluent syntax applied
+          res.body.should.have.property('_id');
+          // recupere l'id du post pour tester le get par id
+          id_created = res.body._id;
+          res.body.owner.should.equal(id_owner);
+          res.body.nom.should.equal("Ma pédale");
+          res.body.description.should.equal("Celle de Tonton");
+          res.body.password.should.have.property("users");
+          // TODO : enfants
+          res.body.password.should.have.property("effets");
+          done();
+        });
+    });
+
+    // TEST PUT
+    it('should correctly update a note', function (done) {
+      pedalBody.nom = 'Ma pédale modifée';
+      // TODO : Ajouter des users
+      request(URL)
+        .put(URL_NOTE + id_created)
+        .send(noteBody)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200) //Status code success
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          // Should.js fluent syntax applied
+          res.body.should.have.property('_id');
+          res.body.nom.should.equal('Ma pédale modifée');
+          done();
+        });
+    });
+
+    // TEST GET PAR ID
+    it('should correctly get a note', function (done) {
+      request(URL)
+        .get(URL_PEDAL + id_created)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200) //Status code success
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          res.body.should.have.property('_id');
+          res.body._id.should.equal(id_created);
+          res.body.nom.should.equal("Ma pédale");
+          res.body.description.should.equal("Celle de Tonton");
+          // TODO : check content
+          res.body.password.should.have.property("effets");
+          res.body.password.should.have.property("users");
+          done();
+        });
+    });
+
+    // TEST DELETE
+    it('should correctly delete a note', function (done) {
+      request(URL)
+        .delete(URL_NOTE + id_created)
+        .expect('Content-type', 'application/json; charset=utf-8')
+        .expect(200) //Status code success
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+          // Should.js fluent syntax applied
+          res.body.should.not.have.property('_id');
+          res.body.should.have.property('message');
+          res.body.message.should.equal('Successfully deleted');
+          done();
+        });
+    });
+  });
 
 });
