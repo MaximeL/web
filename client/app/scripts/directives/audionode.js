07dc76c3ef3cc486adaa1e8709bfd17822e62002
yezide <bezeid.moulaye-ely@etu.unice.fr>
2016-02-01 12:48:51 +0100

    position of node saved (top and left)

--- client/app/scripts/directives/audionode.js
+++ client/app/scripts/directives/audionode.js
@@ -51,8 +51,15 @@ angular.module('webClientSideApp')
             connectorStyle: {strokeStyle: "red", lineWidth: 1},
             connectorHoverStyle: {lineWidth: 2}
           });
-
-          jsPlumb.draggable(element);
+          jsPlumb.draggable("" + scope.nodeId);
+          console.log("position -----> ");
+          element.bind("click", function(){
+            console.log("clickkkkk");
+            console.log(element.position());
+            scope.top = element.position().top;
+            scope.left = element.position().left;
+          });
+          console.log("<-------- position");
         }
       }
     };
