863205ff459fd1337d540f5457e23b2009a5d657
MaximeL <maxime.lozach@easytrust.com>
2016-02-07 22:38:55 +0100

    quickfix

--- client/app/scripts/services/soundnode/convolver.js
+++ client/app/scripts/services/soundnode/convolver.js
@@ -30,7 +30,7 @@ angular.module('webClientSideApp')
       this.input.gain.value = 1;
 
       var soundRequest = new XMLHttpRequest();
-      soundRequest.open('GET', config.apiURL+ config.samples +'irHall', true);
+      soundRequest.open('GET', config.apiURL+ config.samples +'irHall.ogg', true);
       soundRequest.setRequestHeader('Access-Control-Allow-Origin', '*');
       soundRequest.setRequestHeader('Access-Control-Allow-Methods', 'ANY');
       soundRequest.responseType = "arraybuffer";
