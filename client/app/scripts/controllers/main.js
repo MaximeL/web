2e96f5119d17cfb7e7d17b49544e9d919b97e56f
Romain Palmero <romainpalmero.dev@gmail.com>
2016-02-09 17:47:39 +0100

    Fucking fix on gravatar

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -146,76 +146,10 @@ angular.module('webClientSideApp')
 
     // permet de hash un email
     $scope.hashEmail = function (email) {
-      var str = md5.createHash(email);
-      var url = "http://www.gravatar.com/avatar/" + str + ".jpg?s=30&d=retro";
-      return url;
-    };
-
-    $scope.myPedals = [];
-    $scope.myPedals.push(
-      {
-        "_id": "56a9ecead4b0c99c25e4b2df",
-        "owner": "56a9ecead4b0c99c25e4b2de",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        "name": "Ma pédale modifée",
-        "users": [
-          {"username": "Robert", "email": "truc@mail.com"},
-          {"username": "Jean-henri", "email": "truc1@mail.com"},
-          {"username": "Brigitte", "email": "truc2@mail.com"},
-          {"username": "Jacqueline", "email": "truc3@mail.com"},
-          {"username": "Micheline", "email": "truc4@mail.com"}
-        ],
-        "comments": [
-          {
-            "author": "56a9ecead4b0c99c25e4b2de",
-            "_id": "56a9ecead4b0c99c25e4b2e1"
-          }
-        ],
-        "notes": [
-          {
-            "author": "56a9ecead4b0c99c25e4b2de",
-            "_id": "56a9ecead4b0c99c25e4b2e0"
-          }
-        ],
-        "effets": [
-          {
-            "type": "aze",
-            "suivant": "aze",
-            "precedent": "aze"
-          }
-        ]
-      },
-      {
-        "_id": "56a9CCCad4b0c99c25e4b2df",
-        "owner": "56a9CCCad4b0c99c25e4b2de",
-        "description": "C'est la pédale à tonton",
-        "name": "Ze mega pedale",
-        "users": [
-          {"username": "Brigitte", "email": "truc5@mail.com"},
-          {"username": "Jacqueline", "email": "truc6@mail.com"},
-          {"username": "Micheline", "email": "truc7@mail.com"}
-        ],
-        "comments": [
-          {
-            "author": "56a9ecead4b0c99c25e4b2de",
-            "_id": "56a9ecead4b0c99c25e4b2e1"
-          }
-        ],
-        "notes": [
-          {
-            "author": "56a9ecead4b0c99c25e4b2de",
-            "_id": "56a9ecead4b0c99c25e4b2e0"
-          }
-        ],
-        "effets": [
-          {
-            "type": "aze",
-            "suivant": "aze",
-            "precedent": "aze"
-          }
-        ]
+      if(email === undefined) {
+        return "";
       }
-    );
-
+      return md5.createHash(email);
+    };
 
   });
