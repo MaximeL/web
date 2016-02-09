973e0f8422f0206c09be128a604c7657e623be27
yezide <bezeid.moulaye-ely@etu.unice.fr>
2016-02-09 21:40:37 +0100

    rectif port number

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -23,7 +23,7 @@ angular
   ])
   .constant('config',
     {
-      apiURL: "http://localhost:3000/",
+      apiURL: "http://localhost:3001/",
       samples: "samples/",
       users: "api/users/",
       users_auth: "auth",
